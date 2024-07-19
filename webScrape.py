7e6991e4761555908efeca6c255d771153174f32
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-07-19 15:43:51 +0300

    finding-text

--- webScrape.py
+++ webScrape.py
@@ -3,6 +3,7 @@ import requests
 
 sourceCode = requests.get('https://www.rmit.edu.au/').text
 htmlCode = BeautifulSoup(sourceCode, 'lxml')
-article = htmlCode.find('article')
-print(article.div.a)
+article = htmlCode.find('p', class_ = 'short-desc-gen').text
+# print(article.find('div', class_ = 'row cmp-list'))
+print(article)
 
